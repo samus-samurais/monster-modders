58a44b6164f98cd7570556df827139712cf0c119
Clay Carroll <claycarroll7@gmail.com>
2021-10-08 11:36:10 -0500

    Added(api/user): added api routes to access single user and all users and to create/update a user

---
+++ server/api/user.js
@@ -0,0 +1,45 @@
+const router = require("express").Router();
+const {
+    models: {User}
+} = require("../db");
+
+//middleware
+
+router.get("/", async (req, res, next) => {
+    try {
+        const users = await User.findAll({attributes: ['id', 'username', 'email', 'number_of_wins']});
+        res.json(users);
+    } catch (error) {
+        next(error);
+    }
+});
+
+router.get("/:userId", async (req, res, next) => {
+    try {
+        const user = await User.findByPk(req.params.id, {
+            attributes: ['id', 'username', 'email', 'number_of_wins']
+        });
+        res.json(user);
+    } catch (error) {
+        next(error);
+    }
+});
+
+router.post("/", async (req, res, next) => {
+    try {
+        const user = await User.create(req.body);
+        res.json(user);
+    } catch (error) {
+        next(error)
+    }
+});
+
+router.put("/:userId", async (req, res, next) => {
+    try {
+        const user = await User.findByPk(req.params.id);
+        await user.update(req.body);
+        res.status(200).json(user);
+    } catch (error) {
+        next(error)
+    }
+})
