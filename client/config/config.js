b37ced18db204d902bc438a68a8554ac60b0ac55
Jiefei Wang <wangjfmh@gmail.com>
2021-10-08 17:12:15 -0400

    change the game scene size to 1280*720

--- client/config/config.js
+++ client/config/config.js
@@ -2,8 +2,8 @@ import 'phaser'
 
 export default {
     type: Phaser.AUTO,
-    width: 800,
-    height: 600,
+    width: 1280,
+    height: 720,
     physics: {
         default: 'arcade',
         arcade: {
--- client/scenes/Prototype.js
+++ client/scenes/Prototype.js
@@ -1,3 +1,5 @@
+import Phaser from "phaser";
+
 export default class Prototype extends Phaser.Scene {
     constructor(key) {
         super(key);
