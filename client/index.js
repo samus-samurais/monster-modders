8f5c5f09153efaf72a4279b0b00e86ba920732f4
Clay Carroll <claycarroll7@gmail.com>
2021-10-11 14:39:23 -0500

    There is a lot here... need to get better at committing and pull requests

--- client/index.js
+++ client/index.js
@@ -1,5 +1,7 @@
 import 'phaser'
 import config from './config/config'
+import HomeScene from './scenes/HomeScene'
+import SignupScene from './scenes/SignupScene'
 import Prototype from './scenes/Prototype'
 
 class Game extends Phaser.Game {
@@ -7,10 +9,12 @@ class Game extends Phaser.Game {
     //initializes the game with set configurations in config.js
     super(config)
     //Adds scenes
+    this.scene.add('HomeScene', HomeScene)
+    this.scene.add('SignupScene', SignupScene)
     this.scene.add('Prototype', Prototype)
 
     //Starts prototype (we'll replace this later)
-    this.scene.start('Prototype')
+    this.scene.start('HomeScene')
   }
 }
 
