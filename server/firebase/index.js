ba5619903efb4f4a12356f5b8b0ce2c94f68eb26
Clay Carroll <claycarroll7@gmail.com>
2021-10-11 12:03:56 -0500

    removed(server/): removed everything in server directory to make way for firebase implementation. Currently have firebaseConfig set up

---
+++ server/firebase/index.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDzDT4WNg-Z_iX2pF1tt6dn6wLz-KU2QI0",
+  authDomain: "monster-modders.firebaseapp.com",
+  projectId: "monster-modders",
+  storageBucket: "monster-modders.appspot.com",
+  messagingSenderId: "377619460143",
+  appId: "1:377619460143:web:6413eb21e95cb1bd8d96da"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
