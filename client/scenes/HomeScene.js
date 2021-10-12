c3a072a0b8162b0ef29ffd2be50273c4472b4b8f
Clay Carroll <claycarroll7@gmail.com>
2021-10-12 12:15:51 -0500

    feat(scenes/HomeScene): added music to play on the homescene when the site is loaded... music will play continuously until user presses a different scene.

--- client/scenes/HomeScene.js
+++ client/scenes/HomeScene.js
@@ -19,17 +19,35 @@ export default class HomeScene extends Phaser.Scene {
         this.load.image('sandboxButton', 'assets/testImage/falseSandboxButton.png');
         this.load.image('multiplayerButton', 'assets/testImage/falseMultiplayerButton.png');
         this.load.image('loginSignupButton', 'assets/testImage/falseLoginSignupButton.png');
+
+        //audio
+        this.load.audio("lobbyMusic", "assets/audio/lobby-music.mp3");
     }
 
     create(){
         this.add.image(640, 360, 'background');
         this.add.image(640, 250, 'logo');
 
+        this.music = this.sound.add("lobbyMusic");
+
+        var musicConfig = {
+          mute: false,
+          volume: 0.2,
+          rate: 1,
+          detune: 0,
+          seek: 0,
+          loop: true,
+          delay: 0
+        }
+
+        this.music.play(musicConfig); //plays music indefinitley
+
         // make sandbox mode button
         // need to navigate to the real sandbox mode
         this.sandboxButton = this.add.image(320, 540, 'sandboxButton').setInteractive();
         this.sandboxButton.on('pointerdown', () => {
           this.scene.stop('HomeScene');
+          this.music.stop(); //stop playing music when this scene is pressed by user
           this.scene.launch('Prototype',{socket: this.socket});
           this.sandboxButton.disableInteractive();
         });
