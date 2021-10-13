0cfdbc3ff49d9fe0524a05bff9d41bd8858cc3b7
Clay Carroll <claycarroll7@gmail.com>
2021-10-13 11:23:54 -0500

    added(assets/audio): added multiple audio files for the game. Mostly background music for the levels. These will want to be played in a looping manner. Also changed the file name of 'jumping' to 'jumping-sound' to adhere to a better naming convention

---
+++ server/firebaseConfig.js
@@ -0,0 +1,10 @@
+module.exports = {
+    apiKey: "AIzaSyDzDT4WNg-Z_iX2pF1tt6dn6wLz-KU2QI0",
+    authDomain: "monster-modders.firebaseapp.com",
+    databaseURL: "https://monster-modders-default-rtdb.firebaseio.com",
+    projectId: "monster-modders",
+    storageBucket: "monster-modders.appspot.com",
+    messagingSenderId: "377619460143",
+    appId: "1:377619460143:web:6413eb21e95cb1bd8d96da"
+};
+  
