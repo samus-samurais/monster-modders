15572132c94bb7be69ce770108614817b5fe5314
William Leet <williamleet@Chrisophers-MBP.lan>
2021-10-20 14:19:09 -0600

    Fixed various room bugs

--- client/scenes/UserProfileScene.js
+++ client/scenes/UserProfileScene.js
@@ -11,14 +11,17 @@ export default class UserProfileScene extends Phaser.Scene {
   }
 
   create() {
-    this.add.image(640, 360, 'background');
+    this.UI = this.add.group();    
 
     // display login user's information
     this.username = this.add.text(450, 150, `Player username: ${this.playerInfo.username}`, { color: 'white', fontFamily: 'Arial', fontSize: '32px '});
+    this.UI.add(this.username);
 
     this.email = this.add.text(450, 250, `Player email: ${this.playerInfo.email}`, { color: 'white', fontFamily: 'Arial', fontSize: '32px '});
+    this.UI.add(this.email);
 
     this.numberOfWins = this.add.text(450, 350, `Player wins: ${this.playerInfo.number_of_wins}`, { color: 'white', fontFamily: 'Arial', fontSize: '32px '});
+    this.UI.add(this.numberOfWins);
 
     // navigate to the game different modes button
     this.sandboxButton = this.add.image(445, 540, 'sandboxButton').setInteractive();
@@ -26,12 +29,14 @@ export default class UserProfileScene extends Phaser.Scene {
       this.scene.start('Sandbox', {socket: this.socket, user: this.playerInfo});
       this.sandboxButton.disableInteractive();
     });
+    this.UI.add(this.sandboxButton);
 
     this.multiplayerButton = this.add.image(765, 540, 'multiplayerButton').setInteractive();
     this.multiplayerButton.on('pointerdown', () => {
-      this.scene.start('LobbyScene', {socket: this.socket, user: this.playerInfo});
+      this.scene.launch('RoomSelector',{socket: this.socket, user: this.playerInfo, prevSceneUI: this.UI});
       this.multiplayerButton.disableInteractive();
     })
+    this.UI.add(this.multiplayerButton);
 
   }
 }
