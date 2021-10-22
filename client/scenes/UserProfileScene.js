f6c5741034f0479a948f52f982aace457ffb4fa9
Jiefei Wang <wangjfmh@gmail.com>
2021-10-22 14:38:37 -0400

    fixed UserProfileScene issue and added the back button

--- client/scenes/UserProfileScene.js
+++ client/scenes/UserProfileScene.js
@@ -11,7 +11,8 @@ export default class UserProfileScene extends Phaser.Scene {
   }
 
   create() {
-    this.UI = this.add.group();    
+    this.add.image(640, 360, 'background');
+    this.UI = this.add.group();
 
     // display login user's information
     this.username = this.add.text(450, 150, `Player username: ${this.playerInfo.username}`, { color: 'white', fontFamily: 'Arial', fontSize: '32px '});
@@ -20,13 +21,10 @@ export default class UserProfileScene extends Phaser.Scene {
     this.email = this.add.text(450, 250, `Player email: ${this.playerInfo.email}`, { color: 'white', fontFamily: 'Arial', fontSize: '32px '});
     this.UI.add(this.email);
 
-    this.numberOfWins = this.add.text(450, 350, `Player wins: ${this.playerInfo.number_of_wins}`, { color: 'white', fontFamily: 'Arial', fontSize: '32px '});
-    this.UI.add(this.numberOfWins);
-
     // navigate to the game different modes button
     this.sandboxButton = this.add.image(445, 540, 'sandboxButton').setInteractive();
     this.sandboxButton.on('pointerdown', () => {
-      this.scene.start('Sandbox', {socket: this.socket, user: this.playerInfo});
+      this.scene.start('Sandbox', {socket: this.socket, user: this.playerInfo, prevSceneUI: this.UI});
       this.sandboxButton.disableInteractive();
     });
     this.UI.add(this.sandboxButton);
@@ -38,5 +36,30 @@ export default class UserProfileScene extends Phaser.Scene {
     })
     this.UI.add(this.multiplayerButton);
 
+    this.goBack();
+
+  }
+
+
+  goBack() {
+    const backButton = this.add
+      .image(this.scale.width - 20, 20, 'backButton')
+      .setScrollFactor(0)
+      .setOrigin(1, 0)
+      .setScale(2);
+    backButton.setInteractive();
+    backButton.on("pointerdown", () => {
+      backButton.setTint(0xFF0000);
+    });
+    backButton.on("pointerover", () => {
+      backButton.setTint(0xFF0000);
+    });
+    backButton.on("pointerout", () => {
+      backButton.clearTint();
+    })
+    backButton.on("pointerup", () => {
+      this.scene.stop("UserProfileScene");
+      this.scene.start("HomeScene", {socket: this.socket, user: this.playerInfo});
+    })
   }
 }
